08d0a6427aaeedadd72c9faebd0a5e855203b14b
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-27 20:00:25 +0300

    Import & implement rocket attributes props.

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,16 +1,26 @@
 import './styles/Rocket.css';
+import PropTypes from 'prop-types';
 
-const Rocket = () => (
-  <li>
+const Rocket = ({
+  id, image, name, description,
+}) => (
+  <div key={id} className="listContainer">
     <div className="imgContainer">
-      <img src="Rocket.png" alt="" className="rocketImg" />
+      <img src={image} alt="" className="rocketImg" style={{ width: '250px' }} />
     </div>
     <div className="details">
-      <h2 className="rocketName">Name</h2>
-      <p className="rocketDispription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, animi. Impedit ipsam deleniti dolores, totam in pariatur voluptates suscipit nisi ducimus? Libero eligendi iure deserunt harum non obcaecati accusantium tempora.</p>
+      <h2 className="rocketName">{name}</h2>
+      <p className="rocketDispription">{description}</p>
       <button type="button" className="reservationBtn">Reserve Rocket</button>
     </div>
-  </li>
+  </div>
 );
+
+Rocket.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Rocket;
