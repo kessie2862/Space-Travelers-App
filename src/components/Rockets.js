d55062e7829d8b034313f083e2f70acd702354a7
akezeth <116703083+akezeth@users.noreply.github.com>
2023-06-27 19:59:36 +0300

    Use useSelector hooks to get rocket data state

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-// import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { fetchRockets } from '../redux/rocket/rocketSlice';
 import Rocket from './Rocket';
 
 function Rockets() {
-  // const rocketItems = useSelector((state) => state.rocket.Rockets);
+  const rocketItems = useSelector((state) => state.rocket.rockets);
   const dispatch = useDispatch();
-  // console.log(rocketItems);
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
   return (
     <div className="container">
-      <Rocket />
-      <Rocket />
-      <Rocket />
+      {rocketItems.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          image={rocket.image}
+          name={rocket.rocket_name}
+          description={rocket.description}
+        />
+      ))}
     </div>
   );
 }
