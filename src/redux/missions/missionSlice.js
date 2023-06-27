a06c228b41de6e14194a333be2f1388961370ee0
kessie <kayp3477@gmail.com>
2023-06-27 13:15:13 +0000

    Add mission-related Redux actions and reducers

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -7,8 +7,20 @@ const missionSlice = createSlice({
     setMissions: (state, action) => {
       state.missions = action.payload;
     },
+    joinMission: (state, action) => {
+      const missionId = action.payload;
+      state.missions = state.missions.map((mission) => (mission.mission_id === missionId
+        ? { ...mission, reserved: true }
+        : mission));
+    },
+    leaveMission: (state, action) => {
+      const missionId = action.payload;
+      state.missions = state.missions.map((mission) => (mission.mission_id === missionId
+        ? { ...mission, reserved: false }
+        : mission));
+    },
   },
 });
 
-export const { setMissions } = missionSlice.actions;
+export const { setMissions, joinMission, leaveMission } = missionSlice.actions;
 export default missionSlice.reducer;
