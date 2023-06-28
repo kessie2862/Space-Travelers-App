3a90460a62f3ae6301c231ea76dda434377448bd
kessie <kayp3477@gmail.com>
2023-06-28 06:52:54 +0000

    Add Profile component to display joined missions

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './styles/Profile.css';
+
 function Profile() {
+  const joinedMissions = useSelector((state) => state.mission
+    .missions.filter((mission) => mission.reserved));
+
   return (
-    <div className="container">
-      <h2>Profile Page</h2>
+    <div className="profile-container">
+      <h2>My Missions</h2>
+      <div className="joined-missions">
+        {joinedMissions.length === 0 ? (
+          <p>No missions joined.</p>
+        ) : (
+          <ul>
+            {joinedMissions.map((mission) => (
+              <li key={mission.mission_id}>{mission.mission_name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
