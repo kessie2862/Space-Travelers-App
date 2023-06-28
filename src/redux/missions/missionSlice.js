9904c79a35d60f7006af145510e53be6bc5e345d
kessie <kayp3477@gmail.com>
2023-06-28 18:32:54 +0000

    Add joinedMissions state and update reducers

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -2,24 +2,33 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const missionSlice = createSlice({
   name: 'mission',
-  initialState: { missions: [] },
+  initialState: {
+    missions: [],
+    joinedMissions: [],
+  },
   reducers: {
     setMissions: (state, action) => {
       state.missions = action.payload;
     },
     joinMission: (state, action) => {
       const missionId = action.payload;
-      state.missions = state.missions.map((mission) => (mission.mission_id === missionId
-        ? { ...mission, reserved: true }
-        : mission));
+      const mission = state.missions.find(
+        (mission) => mission.mission_id === missionId,
+      );
+      if (mission) {
+        mission.reserved = true;
+        state.joinedMissions.push(mission);
+      }
     },
     leaveMission: (state, action) => {
       const missionId = action.payload;
-      state.missions = state.missions.filter(
+      state.missions = state.missions.map((mission) => (mission.mission_id === missionId
+        ? { ...mission, reserved: false }
+        : mission));
+      state.joinedMissions = state.joinedMissions.filter(
         (mission) => mission.mission_id !== missionId,
       );
     },
-
   },
 });
 
