22597830e8a7e203c5d07ba69ddaf7fdd25cc346
kessie <kayp3477@gmail.com>
2023-06-29 13:55:23 +0000

    Removed unwanted comments

--- src/redux/rocket/rocketSlice.js
+++ src/redux/rocket/rocketSlice.js
@@ -16,7 +16,6 @@ const rocketSlice = createSlice({
   name: 'Rockets',
   initialState: {
     rockets: [],
-    // bookedRockets: [],
     isLoading: true,
   },
   reducers: {
@@ -24,19 +23,11 @@ const rocketSlice = createSlice({
       const rocketId = action.payload;
       state.rockets = state.rockets.map((rocket) => (rocket.id === rocketId
         ? { ...rocket, reserved: true } : rocket));
-      // const rocket = state.rockets.find(
-      //   (rocket) => rocket.id === rocketId,
-      // );
-      // if (rocket) {
-      //   rocket.reserved = true;
-      //   state.bookedRockets.push(rocket);
-      // }
     },
     cancelReservation: (state, action) => {
       const rocketId = action.payload;
       state.rockets = state.rockets.map((rocket) => (rocket.id === rocketId
         ? { ...rocket, reserved: false } : rocket));
-      // state.bookedRockets = state.bookedRockets.filter((rocket) => rocket.id !== rocketId);
     },
   },
   extraReducers: (builder) => {
